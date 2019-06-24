82b54d9fd1e7bcfabfe674df8facbadc9a5c57db
zubeir-Abubakar <zubkayare@gmail.com>
2019-06-24 14:34:45 +0300

    import Api key

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
 
-   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
-   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
-   	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+   	NEWS_SOURCES _BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+   	ARTICLES_ BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+   	NEWS_AP I_KEY = os.environ.get('NEWS_API_KEY')
     # SECRET_KEY = os.environ.get('SECRET_KEY')
    	@staticmethod
    	def init_app(app):
@@ -15,7 +15,7 @@ class ProdConfig(Config):
     pass
 
 
-class DevConfig (Config):
+class DevConfig(Config):
     DEBUG = True
 
 config_options = {
