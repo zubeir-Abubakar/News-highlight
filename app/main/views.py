5cec6ae82d753cc461174e4c099f5b0fe2eb76a8
zubeir-Abubakar <zubkayare@gmail.com>
2019-06-24 14:38:23 +0300

    import

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from ..requests import get_sources,get_articles
-from ..models import Sources
+from ..models impo  rt Sources
 
 #views
 @main.route('/')
