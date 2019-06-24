a61291d5a929ac8c7bea19798d1f95e42e5f1add
zubeir-Abubakar <zubkayare@gmail.com>
2019-06-24 14:37:55 +0300

    from path

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 
    	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
    	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
-   	NEWS_AP I_KEY = os.environ.get('NEWS_API_KEY')
+   	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     # SECRET_KEY = os.environ.get('SECRET_KEY')
    	@staticmethod
    	def init_app(app):
