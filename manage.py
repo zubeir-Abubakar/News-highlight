87221fa98479cd5d8421de959d426181c6ed8ca6
zubeir-Abubakar <zubkayare@gmail.com>
2019-06-24 14:57:30 +0300

    test loader

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    unittest.TextTestRunner(  verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
